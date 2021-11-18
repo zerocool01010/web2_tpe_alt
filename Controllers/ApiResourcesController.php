1fbb9b3d3126c84bc266921bcdd27259f23945e6
Manuel Alvarez <manuelalvarez_1993@hotmail.com>
2021-11-18 16:15:35 -0300

    Agrego el route-api, el Router.php al folder libs, el ApiResourcesController.php y el ApiView.php

---
+++ Controllers/ApiResourcesController.php
@@ -0,0 +1,73 @@
+<?php
+require_once "./Models/resourcesModel.php";
+require_once "./Views/ApiView.php";
+
+class ApiResourcesController{
+
+    private $model;
+    private $view;
+
+    function __construct(){
+        $this->model = new resourcesModel();
+        $this->view = new ApiView();
+    }
+
+    /* function obtenerTareas(){
+        $tareas = $this->model->getTasks(); 
+        return $this->view->response($tareas, 200); 
+    }
+
+    function obtenerTarea($params = null) { 
+        $idTarea = $params[":ID"]; 
+        $tarea = $this->model->getTask($idTarea); 
+        if ($tarea) { //si existe...
+            return $this->view->response($tarea, 200); 
+        } else {
+            return $this->view->response("La tarea con el id=$idTarea no existe", 404); 
+        }
+    }
+
+    function eliminarTarea($params = null) { 
+        $idTarea = $params[":ID"];
+        $tarea = $this->model->getTask($idTarea);
+
+        if ($tarea) {
+            $this->model->deleteTaskFromDB($idTarea);
+            return $this->view->response("La tarea con el id=$idTarea fue borrada", 200);
+        } else {
+            return $this->view->response("La tarea con el id=$idTarea no existe", 404);
+        }
+    } */
+
+    function insertReview() { 
+        /* $body = $this->getBody();  */
+        $saludo = "hola";
+        print_r($saludo);
+        $id = $this->model->addReview(); 
+        if ($id != 0) {
+            $this->view->response("La reseña se insertó con el id=$id", 200);
+        } else {
+            $this->view->response("La reseña no se pudo insertar", 500);
+        }
+    }
+
+   /*  function actualizarTarea($params = null) { 
+        $idTarea = $params[':ID'];
+        $body = $this->getBody();
+      
+        $tarea = $this->model->getTask($idTarea);
+
+        if ($tarea) {
+            $this->model->update($idTarea, $body->titulo, $body->descripcion, $body->prioridad, $body->finalizada);
+            $this->view->response("La tarea se actualizó correctamente", 200);
+        } else {
+            return $this->view->response("La tarea con el id=$idTarea no existe", 404);
+        }
+    }
+   
+    private function getBody() {
+        $bodyString = file_get_contents("php://input");
+        return json_decode($bodyString); 
+    } */
+
+}
---
+++ route-api.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'libs/Router.php';
+require_once 'Controllers/ApiResourcesController.php';
+
+$router = new Router();
+
+$router->addRoute('recurso', 'GET', 'ApiResourcesController', 'getReviews');
+$router->addRoute('recurso/:ID', 'GET', 'ApiResourcesController', 'getReview');
+$router->addRoute('recurso/:ID', 'DELETE', 'ApiResourcesController', 'deleteReview');
+$router->addRoute('recurso', 'POST', 'ApiResourcesController', 'insertReview');
+$router->addRoute('recurso/:ID', 'PUT', 'ApiResourcesController', 'updateReview');
+
+$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
