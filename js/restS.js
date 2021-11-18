981199b57a0d33e2663a7707f8674c41f7503bc4
Manuel Alvarez <manuelalvarez_1993@hotmail.com>
2021-11-18 16:13:47 -0300

    Implementada funcion insertReview en restS.js

--- js/restS.js
+++ js/restS.js
@@ -9,7 +9,14 @@ async function insertReview(){
     let review = document.querySelector("#review").value;
     console.log(review);
     try {
-
+        let res = await fetch (API_URL, {
+            "method": "POST",
+            "headers": {"Content-type": "application/json"},
+            "body": JSON.stringify(review)
+        });
+        if(res.status == 201 || res.status == 200){
+            console.log("Datos cargados!");
+        }
     } catch (e){
         console.error();
     }
